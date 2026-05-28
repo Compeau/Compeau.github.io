7bb6227ff9ab4ec24bbd88fac4b5db8c7af6adac
Adam Compeau <adamcompeau@gmail.com>
2026-05-27 22:42:29 -0400

    add files

---
+++ MineCrash/MineCrash.js
@@ -0,0 +1,461 @@
+enchant();
+
+enchant.Sound.enabledInMobileSafari = true;
+
+if(location.protocol == 'file:'){
+    enchant.ENV.USE_WEBAUDIO = false;
+    console.log('1');
+}
+
+window.onload = function () {
+    game = new Game(854, 480);
+    game.fps = 60;
+    game.score = 0;
+	game.topscore = 10000;
+    game.touched = false;
+    game.apressed = false;
+    game.ended = true;
+    game.preload('ship.png');
+    game.preload('bullet.png');
+    game.preload('comet_large.png');
+    game.preload('comet_med.png');
+    game.preload('comet_small.png');
+	game.preload('black.png');
+    game.preload('shoot.wav');
+    game.preload('die.wav');
+    game.preload('explode.wav');
+    game.preload('zombie.png');
+    game.preload('creeper.png');
+    game.preload('arrow_shoot.mp3');
+    game.preload('arrow_hit.mp3');
+    game.preload('steve.png');
+    game.preload('zombie.mp3');
+    game.preload('arrow.png');
+    game.preload('creeper.mp3');
+    game.preload('creeper-explosion.mp3');
+
+game.keybind(37, 'left');
+game.keybind(39, 'right');
+game.keybind(38, 'up');
+game.keybind(40, 'down');
+game.keybind(32, 'space');
+game.keybind(13, 'enter');
+   
+    game.onload = function () {
+        player = new Player(0, (game.height/2)-12);
+        enemies = new Array();
+        game.rootScene.backgroundColor = 'darkgreen';
+        
+        //Create new enemies
+        enemyFrequency = 60;
+        game.rootScene.addEventListener('enterframe', function () {
+            if(game.frame%enemyFrequency == 0 && !game.ended){
+                var y = rand(game.height);
+                var enemy = new Comet(game.width, y, rand(20) - 10, rand(2), 2+(game.score/25000));
+                enemies.push(enemy);
+				if(game.score>10000)
+				{
+					var y = rand(game.height);
+					var enemy = new Comet(game.width, y, rand(20) - 10, rand(2), 3+(game.score/50000));
+					enemies.push(enemy);
+				}
+				if(game.score>25000)
+				{
+					var y = rand(game.height);
+					var enemy = new Comet(game.width, y, rand(20) - 10, rand(2), 4+(game.score/50000));
+					enemies.push(enemy);
+				}
+				if(game.score>50000)
+				{
+					var y = rand(game.height);
+					var enemy = new Comet(game.width, y, rand(20) - 10, rand(2), 2+game.score/25000);
+					enemies.push(enemy);
+				}
+				if(game.score>100000)
+				{
+					var y = rand(game.height);
+					var enemy = new Comet(game.width, y, rand(20) - 10, rand(2), game.score/12000);
+					enemies.push(enemy);
+				}
+				
+                game.rootScene.removeChild(scoreLabel);
+                game.rootScene.addChild(scoreLabel);
+                game.rootScene.removeChild(topScoreLabel);
+                game.rootScene.addChild(topScoreLabel);
+            }
+            
+            /*if(window.wiiu){
+                if(game.frame%5 == 0 && game.input.a && !game.ended){
+                	var s = new PlayerShoot(player.x, player.y);
+                }
+            }*/
+            
+            scoreLabel.score = game.score;
+			if(game.score>game.topscore){
+				game.topscore = game.score;
+				topScoreLabel.score = game.topscore;
+			}
+			else{
+				topScoreLabel.score = game.topscore;
+			}
+        });
+        scoreLabel = new ScoreLabel(4, 4);
+        game.rootScene.addChild(scoreLabel);
+		topScoreLabel = new TopScoreLabel(310, 4);
+		game.rootScene.addChild(topScoreLabel);
+
+		gameOver = new MutableText(350, 150, 300);
+		gameOver.text = "GAME OVER";
+		
+		restart = new MutableText(280, 250, 500);
+		/*if(window.wiiu){
+			restart.text = "PRESS A TO RESTART";
+		}
+		else*/
+        {
+			restart.text = "TAP/CLICK/ENTER TO RESTART";
+		}
+		
+		credits = new MutableText(480, 380, game.height);
+		credits.text = "CREATED BY ADAM COMPEAU";
+		
+		credits2 = new MutableText(10, 380, game.height);
+		credits2.text = "CREATED FOR ANDREW";
+		
+		intro = new MutableText(280, 200, game.height);
+		/*if(window.wiiu){
+			intro.text = "PRESS A TO START";
+        }
+		else*/
+        {
+			intro.text = "TAP/CLICK/ENTER TO START";
+		}
+		game.rootScene.addChild(intro);
+		game.rootScene.addChild(credits);
+		game.rootScene.addChild(credits2);
+    };
+    game.start();
+};
+
+var Player = enchant.Class.create(enchant.Sprite, {
+    initialize: function (x, y) {
+        enchant.Sprite.call(this, 60, 104);
+        this.image = game.assets['steve.png'];
+        this.x = x;
+        this.y = y;
+        this.frame = 0;
+
+        game.rootScene.addEventListener('touchstart', function (e) {
+            player.y = e.y;
+            game.touched = true;
+			if(game.ended)
+			{
+				game.rootScene.removeChild(gameOver);
+				game.rootScene.removeChild(restart);
+				game.rootScene.removeChild(credits);
+				game.rootScene.removeChild(credits2);
+				game.rootScene.removeChild(intro);
+				scoreLabel.x = 4;
+                scoreLabel.y = 4;
+				game.score = 0;
+				game.ended = false;
+			}
+        });
+        game.rootScene.addEventListener('touchmove', function (e) {
+            player.y = e.y;
+        });
+        game.rootScene.addEventListener('touchend', function (e) {
+            player.y = e.y;
+            game.touched = false;
+        });
+
+        this.addEventListener('enterframe', function () {
+            if(!window.wiiu){
+            	if(game.touched && game.frame%5 == 0 && !game.ended){
+                	var s = new PlayerShoot(player.x+20, player.y+30);
+                }
+            }
+			
+			if (this.x > game.width-this.width) {
+				this.x = game.width-this.width;
+            }
+			if (this.x < 0) {
+				this.x = 0;
+            }
+			if (this.y > game.height-this.height) {
+                //this.direction = -this.direction;
+                this.y = game.height-this.height;
+            }
+            if (this.y < 0) {
+                //this.direction = -this.direction;
+                this.y = 0;
+            }
+			
+            // Check if an enemy hit us, ending the game
+			for (var i in enemies) {
+                if (enemies[i].intersect(this)) {
+					var lefthit = this.x+10;
+					var righthit = this.x+this.width-40;
+					var tophit = this.y+25;
+					var bottomhit = this.y+this.height-25;
+					if(lefthit>enemies[i].x+enemies[i].width || //enemy is to the left of us
+						enemies[i].x>righthit || //enemy is to the right of us
+						bottomhit<enemies[i].y ||//enemy is below us
+						tophit>enemies[i].y+enemies[i].height)//enemy is above us
+						{}
+                    else{
+						game.ended = true;
+						game.assets['creeper-explosion.mp3'].clone().play();
+						
+						game.rootScene.addChild(gameOver);
+                        
+						scoreLabel.x = 350;
+                        scoreLabel.y = 180;
+						
+						game.rootScene.addChild(restart);
+						game.rootScene.addChild(credits);
+						game.rootScene.addChild(credits2);
+			    
+                        
+						for (var i in enemies) {
+                            enemies[i].remove();
+							var y = rand(game.height);
+							var enemy = new Comet(3000, y, rand(90) - 45, 0);
+							enemy.moveSpeed = 0;
+							enemies[i] = enemy;
+							enemies[i].remove();
+                        }
+                        
+						game.ended = true;
+					}
+                }
+            }
+
+			if(game.input.left && !game.input.right){
+			    player.x -= 5;
+			}
+			else if(game.input.right && !game.input.left){
+			    player.x += 5;
+			}
+			
+			if(game.input.up && !game.input.down){
+			    player.y -= 5;
+			}
+			else if(game.input.down && !game.input.up){
+			    player.y += 5;
+			}
+
+		
+                	if(game.input.space && game.frame%5 == 0 && !game.ended)
+                    {
+                        var s = new PlayerShoot(player.x+20, player.y+30);
+                    }
+					if(game.input.enter && game.ended)
+					{
+						game.rootScene.removeChild(gameOver);
+						game.rootScene.removeChild(restart);
+						game.rootScene.removeChild(credits);
+						game.rootScene.removeChild(credits2);
+						game.rootScene.removeChild(intro);
+						scoreLabel.x = 4;
+                        scoreLabel.y = 4;
+						game.score = 0;
+						game.ended = false;
+					}
+		
+			/*if(window.wiiu){
+				player.x += game.input.lstick.x * 10;
+				player.y += game.input.lstick.y * 10;
+                if(game.input.a){
+                	if(game.apressed == false && !game.ended)
+                    {
+                		var s = new PlayerShoot(player.x, player.y);
+                    }
+					if(game.apressed == false && game.ended)
+					{
+						game.rootScene.removeChild(gameOver);
+						game.rootScene.removeChild(restart);
+						game.rootScene.removeChild(credits);
+						game.rootScene.removeChild(credits2);
+						game.rootScene.removeChild(intro);
+						scoreLabel.x = 4;
+                        scoreLabel.y = 4;
+						game.score = 0;
+						game.ended = false;
+					}
+                	game.apressed = true;
+                }
+                else
+                {
+                	game.apressed = false;
+                }
+			}*/
+        });
+
+        game.rootScene.addChild(this);
+    }
+});
+
+
+var Comet = enchant.Class.create(enchant.Sprite, {
+    initialize: function (x, y, direction, type, speed) {
+        this.type = type;
+
+        if (type == 0) {
+            enchant.Sprite.call(this, 62, 104);
+            this.image = game.assets['zombie.png'];
+            if(!game.ended){
+     		game.assets['zombie.mp3'].clone().play();
+            }
+        } else if (type == 1) {
+            enchant.Sprite.call(this, 51, 104);
+            this.image = game.assets['creeper.png'];
+            if(!game.ended){
+     		game.assets['creeper.mp3'].clone().play();
+            }
+        }
+
+        this.x = x;
+        this.y = y;
+        this.frame = 3;
+
+        //Sets rotation angle.
+        this.omega = 0;
+
+        //Set movement angle and movement speed (pixels per frame).
+        this.direction = direction;
+        this.moveSpeed = speed;
+
+        this.addEventListener('enterframe', function () {
+            this.move();
+            if (this.x > game.width || this.x < -this.width) {
+				//this.moveSpeed = 0;
+				this.x = game.width;
+                //this.remove();
+            }
+            if (this.y > game.height-this.height) {
+                this.direction = -this.direction;
+                this.y = game.height-this.height;
+            }
+            if (this.y < 0) {
+                this.direction = -this.direction;
+                this.y = 0;
+            }
+
+            for (var i in enemies) {
+                if (enemies[i].intersect(this) && enemies[i] != this) {
+                    this.direction = -this.direction;
+                    if (this.y > enemies[i].y) {
+                        this.y += 2;
+                    }
+                    else {
+                        this.y -= 2;
+                    }
+                }
+            }
+        });
+        game.rootScene.addChild(this);
+    },
+    move: function () {
+        this.direction += this.omega;
+        this.x -= this.moveSpeed * Math.cos(this.direction / 180 * Math.PI);
+        this.y += this.moveSpeed * Math.sin(this.direction / 180 * Math.PI)
+    },
+    remove: function () {
+        game.rootScene.removeChild(this);
+        delete enemies[this.key];
+    }
+});
+
+var Shoot = enchant.Class.create(enchant.Sprite, {
+    initialize: function (x, y, direction) {
+        enchant.Sprite.call(this, 40, 16);
+        this.image = game.assets['arrow.png'];
+        this.x = x+35;
+        this.y = y+12;
+        this.frame = 1;
+        this.direction = direction;
+        this.moveSpeed = 25;
+        this.addEventListener('enterframe', function () {
+            this.x += this.moveSpeed * Math.cos(this.direction);
+            this.y += this.moveSpeed * Math.sin(this.direction);
+            if(this.y > game.height || this.x > game.width-this.width || this.x < -this.width || this.y < -this.height) {
+                this.remove();
+            }
+            if(game.ended)
+            {
+                this.remove();
+            }
+        });
+		game.assets['arrow_shoot.mp3'].clone().play();
+        game.rootScene.addChild(this);
+    },
+    remove: function () {
+        game.rootScene.removeChild(this);
+        delete this;
+    }
+});
+
+var PlayerShoot = enchant.Class.create(Shoot, {
+    initialize: function (x, y) {
+        Shoot.call(this, x, y, 0);
+        this.addEventListener('enterframe', function () {
+            for (var i in enemies) {
+                if (enemies[i].intersect(this)) {
+                    this.x = 4000;
+                    this.remove();
+                    game.score += 500;
+                    game.assets['arrow_hit.mp3'].clone().play();
+                    enemies[i].remove();
+                    var y = rand(game.height);
+                    var enemy = new Comet(3000, y, rand(90) - 45, rand(2), 0);
+                    enemies[i] = enemy;
+                    enemies[i].remove();
+                }
+            }
+        });
+    }
+});
+
+var TopScoreLabel = enchant.Class.create(enchant.ui.MutableText, {
+    initialize: function(x, y) {
+        enchant.ui.MutableText.call(this, 0, 0);
+        switch (arguments.length) {
+            case 2:
+                this.y = y;
+                this.x = x;
+                break;
+            case 1:
+                this.x = x;
+                break;
+            default:
+                break;
+        }
+        this._score = 0;
+        this._current = 0;
+        this.easing = 2.5;
+        this.text = this.label = 'TOP SCORE:';
+        this.addEventListener('enterframe', function() {
+            if (this.easing === 0) {
+                this.text = this.label + (this._current = this._score);
+            } else {
+                var dist = this._score - this._current;
+                if (0 < dist) {
+                    this._current += Math.ceil(dist / this.easing);
+                } else if (dist < 0) {
+                    this._current += Math.floor(dist / this.easing);
+                }
+                this.text = this.label + this._current;
+            }
+        });
+    },
+
+    score: {
+        get: function() {
+            return this._score;
+        },
+        set: function(newscore) {
+            this._score = newscore;
+        }
+    }
+});
